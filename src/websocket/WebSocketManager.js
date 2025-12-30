6107adfc03e3a01630aa5084c6dfd97edd1b10ff
saif-cybernetikz <saiful@cybernetikz.com>
2025-12-30 19:31:01 +0600

    updated

--- src/websocket/WebSocketManager.js
+++ src/websocket/WebSocketManager.js
@@ -20,7 +20,7 @@ class WebSocketManager {
   initialize(httpServer) {
     this.io = new Server(httpServer, {
       cors: {
-        origin: ["https://rze.co", "https://backend.rze.co"],
+        origin: ["https://rze.co", "https://backend.rze.co","http://rze.co", "http://backend.rze.co", "http://127.0.0.1:5000", "http://127.0.0.1:3000"],
         methods: ["GET", "POST"],
         credentials: true,
       },
